5d30b7e73392872515672732414991325c9db26b
Nicole Redel <n.redel@microsoft.wsei.edu.pl>
2019-02-27 20:24:35 +0100

    koniec zajec (localStorage)

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let notes = [];
 
+let titleHtmlNode;
+let contentHtmlNode;
+
 class Note {
     constructor(title, content) {
         this.title = title;
         this.content = content;
+        this.date = Date.now();
     }
 }
 
 document.addEventListener('DOMContentLoaded', appStart);
-
 function appStart() {
     const btnSubmit = document.querySelector('#newNoteBtn');
+    // pobierz tytuł i treść z formularza
+    titleHtmlNode = document.querySelector('#title');
+    contentHtmlNode = document.querySelector('#content');
     btnSubmit.addEventListener('click', addNewNote);
+    getNotesFromLocalStorage();
+}
+
+function getNotesFromLocalStorage() {
     notes = JSON.parse(localStorage.getItem('notes'));
     // notes.forEach(element => {
     //     const newNote = document.createElement('div');
@@ -22,44 +32,74 @@ function appStart() {
     //     const notesConteiner = document.querySelector('main');
     //     notesConteiner.appendChild(newNote);
     // });
-    notes.forEach(note => {
-        const newNote = document.createElement('div');
-        newNote.classList.add('note');
-        newNote.innerHTML =
-            `<h2>${note.title}</h2>
-             <section>${note.content}</section>`;
-        const notesConteiner = document.querySelector('main');
-        notesConteiner.appendChild(newNote);
-    });
+    if (notes && notes.length) {
+    notes.forEach(note => createDivNote(note));
+    } else {
+        notes = [];
+    }
 }
 
 function addNewNote(event) {
     // event.preventDefault(); dodanie w html do button 'type="button"' usuwa jego domyślne niechciane zdarzenie
-    // pobierz tytuł i treść z formularza
-    const title = document.querySelector('#title');
-    const content = document.querySelector('#content');
+    
     // nowy obiekt notatki
-    const note = new Note(title.value, content.value);
-
+    const note = new Note(titleHtmlNode.value, contentHtmlNode.value);
     // const note = {
     //     title: title.value,
     //     content: content.value
     // };
 
-    // dodajemy notatkę do tablicy
-    notes.push(note);
-    // zapisujemy w localStorage
-    localStorage.setItem('notes', JSON.stringify(notes));
+    // dodajemy notatkę do tablicy i zapisujemy w localStorage
+    addToStorage(note);
     // stwórz diva z notatką
+    createDivNote(note);
+
+    cleanForm();
+}
+
+function cleanForm() {
+    titleHtmlNode.value = '';
+    contentHtmlNode.value = '';
+}
+
+function addToStorage(note) {
+    notes.push(note);
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function createDivNote(note) {
     const newNote = document.createElement('div');
     newNote.classList.add('note');
-    // wepchnij do diva tytuł i treść
-    newNote.innerHTML =
-        `<h2>${note.title}</h2>
-        <section>${note.content}</section>`;
-    // dołącz notatkę do worka z notatkami (main)
+    newNote.id = `id${note.date}`;
+
+    const removeBtn = document.createElement('i');
+    removeBtn.className = 'fas fa-times';
+    removeBtn.addEventListener('click', removeDivNote);
+    newNote.appendChild(removeBtn);
+
+    const header = document.createElement('h2');
+    header.innerHTML = note.title;
+    const section = document.createElement('section');
+    section.innerHTML = note.content;
+    newNote.appendChild(header);
+    newNote.appendChild(section);
+
+    const formattedDate = new Date(note.date);
+    const date = document.createElement('div');
+    date.innerHTML = formattedDate.toLocaleString();
+    newNote.appendChild(date);
+
     const notesConteiner = document.querySelector('main');
     notesConteiner.appendChild(newNote);
-    title.value = '';
-    content.value = '';
+}
+
+function removeDivNote() {
+    const noteDiv = this.parentElement;
+    const notesContainer = noteDiv.parentElement;
+    notesContainer.removeChild(noteDiv);
+}
+
+function removeNote() {
+    localStorage.removeItem()
+    removeDivNote();
 }
