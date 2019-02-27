287c0cb37d1253672a9d0c20afe97c0493d942e4
Nicole Redel <n.redel@microsoft.wsei.edu.pl>
2019-02-27 18:31:06 +0100

    przerwa (localStorage!)

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
+let notes = [];
+
+class Note {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', appStart);
+
+function appStart() {
     const btnSubmit = document.querySelector('#newNoteBtn');
     btnSubmit.addEventListener('click', addNewNote);
-    function addNewNote(event) {
-        //pobierz tytul i tresc z formularza
-        const title = document.querySelector('#title');
-        const content = document.querySelector('#content');
-        //stworz diva z notatka
+    notes = JSON.parse(localStorage.getItem('notes'));
+    // notes.forEach(element => {
+    //     const newNote = document.createElement('div');
+    //     newNote.classList.add('note');
+    //     newNote.innerHTML =
+    //         `<h2>${element.title}</h2>
+    //          <section>${element.content}</section>`;
+    //     const notesConteiner = document.querySelector('main');
+    //     notesConteiner.appendChild(newNote);
+    // });
+    notes.forEach(note => {
         const newNote = document.createElement('div');
         newNote.classList.add('note');
-        //wepchnij do diva tytul i tresc
         newNote.innerHTML =
-            `<h2>${title.value}</h2>
-                <section>${content.value}</section>`;
-        //dolacz notatke do worka z notatkami (main)
+            `<h2>${note.title}</h2>
+             <section>${note.content}</section>`;
         const notesConteiner = document.querySelector('main');
         notesConteiner.appendChild(newNote);
-        title.value = '';
-        content.value = '';
-        event.preventDefault();
-    }
-})
+    });
+}
+
+function addNewNote(event) {
+    // event.preventDefault(); dodanie w html do button 'type="button"' usuwa jego domyślne niechciane zdarzenie
+    // pobierz tytuł i treść z formularza
+    const title = document.querySelector('#title');
+    const content = document.querySelector('#content');
+    // nowy obiekt notatki
+    const note = new Note(title.value, content.value);
+
+    // const note = {
+    //     title: title.value,
+    //     content: content.value
+    // };
+
+    // dodajemy notatkę do tablicy
+    notes.push(note);
+    // zapisujemy w localStorage
+    localStorage.setItem('notes', JSON.stringify(notes));
+    // stwórz diva z notatką
+    const newNote = document.createElement('div');
+    newNote.classList.add('note');
+    // wepchnij do diva tytuł i treść
+    newNote.innerHTML =
+        `<h2>${note.title}</h2>
+        <section>${note.content}</section>`;
+    // dołącz notatkę do worka z notatkami (main)
+    const notesConteiner = document.querySelector('main');
+    notesConteiner.appendChild(newNote);
+    title.value = '';
+    content.value = '';
+}
